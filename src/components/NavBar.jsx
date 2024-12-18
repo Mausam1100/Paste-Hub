ba9bac25318b778377f5716acea6f4a3599471df
Mausam Baduwal <mausambaduwal303@gmail.com>
2024-12-18 19:28:55 +0545

    final commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className='bg-slate-800 py-3 sticky top-0'>
         <div className='width-margin flex items-center justify-between'>
-            <h1 className='text-white text-2xl font-bold'>NoteHub</h1>
+            <Link to='/'><h1 className='text-white text-2xl font-bold'>NoteHub</h1></Link>
             <ul className='flex text-white font-semibold text-xl gap-x-7'>
                 <li><NavLink to='/' className={({ isActive }) => isActive ? 'active-btn' : ''}>Home</NavLink></li>
                 <li><NavLink to='/pastes' className={({ isActive }) => isActive ? 'active-btn' : ''}>Paste</NavLink></li>
